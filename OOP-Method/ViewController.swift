7ec7f57091c03d1e9984aacaea83dbf33fc06806
ridolud <faridho.lutfi@gmail.com>
2019-05-15 13:21:45 +0700

    Initial Commit

---
+++ OOP-Method/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OOP-Method
+//
+//  Created by Faridho Luedfi on 15/05/19.
+//  Copyright © 2019 ridolud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
